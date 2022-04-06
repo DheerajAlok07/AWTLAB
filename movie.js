0219469df489539963b1aa42b8b282df5cefd36f
aryan <aryantechie21@gmail.com>
2022-04-06 14:24:59 +0530

    ff

--- movie.js
+++ movie.js
@@ -1,13 +1,13 @@
 async function movies()
 {
     let cityname=document.getElementById("test");
-    let url='https://imdb-api.com/en/API/SearchTitle/k_p5b5jy25/'+cityname.value;
+    let url='https://imdb-api.com/en/API/MostPopularMovies/k_p5b5jy25';
  const res=await fetch(url);
  let data=await res.json();
  console.log(data);
  
-let {results}=data;
-results.forEach(element => {
-    let{title,year}=element;
-    console.log("movie name is"+title+"and year is"+year);
+let {items}=data;
+items.forEach(element => {
+    let{title,rank}=element;
+    console.log("movie name is"+title+"and rank is"+rank);
 });
